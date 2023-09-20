fa111b39ba344b6964bd5f658ad6b80b79f3960e
Tamilore <findtamilore@gmail.com>
2023-09-20 01:34:30 +0100

    Update

--- prints.c
+++ prints.c
@@ -10,17 +10,14 @@
 int prints(va_list list, int chars_printed)
 {
 	char *s = va_arg(list, char *);
-	int len, i = 0;
-
-	len = _strlen(s) - 1;
 
 	if (*s == '\0')
 		return (-1);
 
-	while (i < len)
+	while (*s != '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar(*s);
+		s++;
 		chars_printed++;
 	}
 	return (chars_printed);
